86877cf0335ee501f48b7e43d632049658fb42a9
Diederik Robert marie boeren <drobert-@c1r18s3.42lisboa.com>
2022-05-16 12:58:57 +0100

    Implemented simple pipe

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -2,31 +2,44 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void parent_process(int *p)
+void parent_process(int *p, char **envp)
 {
-	printf("PARENT fd: %d\n", *p);
+	close(p[1]);
+	dup2(*p, STDIN_FILENO);
+
+	char str[] = "wc";
+	char **ls = malloc(sizeof (char *));
+	*ls = str;
+	execve("/usr/bin/wc", ls, envp);
+	free(ls);
+	close(p[0]);
 }
 
-void child_process(int *p)
+void child_process(int *p, char **envp)
 {
-	printf("CHILD fd: %d\n", *(p + 1));
+	close(p[0]);
+	dup2(p[1], STDOUT_FILENO);
+
+	char str[] = "ls";
+	char **ls = malloc(sizeof (char *));
+	*ls = str;
+	execve("/bin/ls", ls, envp);
+	free(ls);
+	close(p[1]);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
-//	char str[] = "ls";
-//	char **ls = malloc(sizeof (char *));
-//	*ls = str;
-//	execve("/bin/ls", ls, envp);
-
 	int p[2];
 	pipe(p);
 	int id = fork();
 	if (id < 0)
 		exit(1);
 	else if (id == 0)
-		child_process(p);
-	else
-		parent_process(p);
+		child_process(p, envp);
+	else {
+		wait(0);
+		parent_process(p, envp);
+	}
 	printf("argc: %d, name: %s\n", argc, *argv);
 }
