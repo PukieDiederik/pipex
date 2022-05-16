86220d4f1722a3830ad6f4ce0e457bf38565cfd8
Diederik Robert marie boeren <drobert-@c1r18s3.42lisboa.com>
2022-05-16 12:40:28 +0100

    Added simple forking

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void parent_process(int *p)
+{
+	printf("PARENT fd: %d\n", *p);
+}
+
+void child_process(int *p)
+{
+	printf("CHILD fd: %d\n", *(p + 1));
+}
 
 int main(int argc, char **argv)
 {
-	printf("argc: %d\nname: %s", argc, *argv);
+//	char str[] = "ls";
+//	char **ls = malloc(sizeof (char *));
+//	*ls = str;
+//	execve("/bin/ls", ls, envp);
+
+	int p[2];
+	pipe(p);
+	int id = fork();
+	if (id < 0)
+		exit(1);
+	else if (id == 0)
+		child_process(p);
+	else
+		parent_process(p);
+	printf("argc: %d, name: %s\n", argc, *argv);
 }
