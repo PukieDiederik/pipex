4b6a6885e5566f916e3bbc814022c7aa27b60d6d
Diederik Robert marie boeren <drobert-@c1r18s3.42lisboa.com>
2022-05-18 12:34:35 +0100

    Added new error function

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -5,6 +5,8 @@
 #include <sys/wait.h>
 #include <sys/fcntl.h>
 
+void print_error(int errno, char *errstr);
+
 void clear_split(char **split)
 {
 	int i;
@@ -65,55 +67,77 @@ void execute(char *cmd, char **envp)
 {
 	char **argv = ft_split(cmd, ' ');
 	if (!argv)
-		exit(3);
+		print_error(5, "Command splitting went wrong");
 	char *program = get_path(argv[0], envp);
 	if (!program)
-		exit(3);
+		print_error(5, "Could not find/create path");
 	execve(program, argv, envp);
 	clear_split(argv);
 	free(program);
-	exit(4);
+	exit(2);
+}
+
+void set_fds(int i, int argc, char **argv, int *p)
+{
+	int fd;
+
+	if (i == 2)
+	{
+		close(p[0]);
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			print_error(6, "Could not open infile for reading");
+			dup2(fd, STDIN_FILENO);
+		close(fd);
+	}
+	if (i != argc - 2)
+	{
+		dup2(p[1], STDOUT_FILENO);
+	}
+	else
+	{
+		fd = open (argv[argc - 1], O_WRONLY);
+		if (fd < 0)
+			print_error(6, "Could not open outfile for reading");
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+	close(p[1]);
+}
+
+void print_error(int errno, char *errstr)
+{
+	write(1, errstr, ft_strlen(errstr));
+	exit(errno);
 }
 
 int main(int argc, char **argv, char **envp)
 {
 	int id;
-	int i = 1;
+	int i;
+	int status;
 
-	if (argc < 4)
-		exit(1);
+	if (argc < 5)
+		print_error(3, "Not enough arguments");
 	if (access(argv[1], R_OK) | access(argv[argc - 1], R_OK))
-		exit(2);
+		print_error(4, "Infile or outfile does not exist or cannot be read");
+	i = 1;
 	while (++i < argc - 1)
 	{
 		int p[2];
 		pipe(p);
 		id = fork();
 		if (id < 0)
-			exit(4);
+			print_error(1, "Fork failed");
 		if (id == 0)
 		{
-			if (i == 2)
-			{
-				int fd = open(argv[1], O_RDONLY);
-				dup2(fd, STDIN_FILENO);
-				close(fd);
-			}
-			if (i != argc - 2)
-			{
-				dup2(p[1], STDOUT_FILENO);
-			}
-			else
-			{
-				int fd = open (argv[argc - 1], O_WRONLY);
-				dup2(fd, STDOUT_FILENO);
-				close(fd);
-			}
-			close(p[1]);
+			set_fds(i, argc, argv, p);
 			execute(argv[i], envp);
 		}
 		else {
-			waitpid(id, NULL, 0);
+			wait(&status);
+			if (WIFEXITED(status) && WEXITSTATUS(status))
+				print_error(2, "Program did not execute correctly");
 			dup2(p[0], STDIN_FILENO);
 			close(p[0]);
 			close(p[1]);
